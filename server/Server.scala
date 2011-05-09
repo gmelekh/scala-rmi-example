7109e6f5d8a5f5df5390dacedbf8d44b2e8dc499
John Barker <jebarker@gmail.com>
2011-05-09 20:48:32 +0800

    Server now extends Application, and catches permission errors to print a more friendly warning

--- server/Server.scala
+++ server/Server.scala
@@ -1,7 +1,18 @@
-object Server {
+// Security
+import java.security.Permission
+
+object Server extends Application {
   val configFile = "evaluator.yaml"
 
-  def main(args: Array[String]) : Unit = {
+  try {
     val Evaluator = new EvaluatorImpl(configFile)
+
+  } catch {
+    case e: java.security.AccessControlException =>
+      missingPermissionError(e.getMessage, e.getPermission)
+  }
+
+  private def missingPermissionError(msg: String, permission: Permission) {
+    println("Unable to perform operation, reason: " + msg + " missing permission " + permission.toString())
   }
 }
